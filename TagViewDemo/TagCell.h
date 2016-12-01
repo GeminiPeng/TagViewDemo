8b3b69d87cd5fcf0fd1eaf26db4cebd3c895b6eb
abcdpengbo123 <18321507001@163.com>
2016-12-01 13:55:32 +0800

    创建collectionView和cell

---
+++ TagViewDemo/TagCell.h
@@ -0,0 +1,13 @@
+//
+//  TagCell.h
+//  TagViewDemo
+//
+//  Created by Pengbo on 2016/12/1.
+//  Copyright © 2016年 Pengbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagCell : UICollectionViewCell
+
+@end
---
+++ TagViewDemo/TagCell.m
@@ -0,0 +1,13 @@
+//
+//  TagCell.m
+//  TagViewDemo
+//
+//  Created by Pengbo on 2016/12/1.
+//  Copyright © 2016年 Pengbo. All rights reserved.
+//
+
+#import "TagCell.h"
+
+@implementation TagCell
+
+@end
